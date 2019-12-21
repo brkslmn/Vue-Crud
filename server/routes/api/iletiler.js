889fa99df350afe2b921a48412ee8ccc8c6f1c3e
Burak Salman <buraksalman84@gmail.com>
2019-12-21 15:21:36 +0300

    ilk uzak mongodb bağlantısı

--- server/routes/api/iletiler.js
+++ server/routes/api/iletiler.js
@@ -3,15 +3,35 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('HELLO');
-});
+router.get('/',async(req, res) => {
+    
+    const iletiler = await loadIletilerCollection();
+    res.send(await iletiler.find({}).toArray()); 
+
+})
+
+
+// router.get('/', (req, res) => {
+//     res.send('HELLO');
+// });
 
 //ileti ekle (ADD)
 
 
 //ileti sil (DELETE)
 
+async function loadIletilerCollection(){
+    const client = await mongodb.MongoClient.connect
+    {
+        'mongodb+srv://brkslmn:<password>@cluster0-vdbgv.mongodb.net/test?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+
+    }
+    return client.db('deneme').collection('iletiler');
+};
 
 
 module.exports = router;
